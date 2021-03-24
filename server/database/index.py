34a57267fc8fbbdbe4fab0e48a12cb86e85b59fe
Nalin Ahuja <nalinahuja22@gmail.com>
2021-03-24 14:33:22 -0400

    Project Backup

--- server/database/index.py
+++ server/database/index.py
@@ -1,9 +1,9 @@
- import mysql.connector
+import mysql.connector
 
- mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword"
+mydb = mysql.connector.connect(
+ host="localhost",
+ user="yourusername",
+ password="yourpassword"
 )
 
 mycursor = mydb.cursor()
