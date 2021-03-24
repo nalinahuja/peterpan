72debfce1316ee63478481b07d165f437e646e39
Nalin Ahuja <nalinahuja22@gmail.com>
2021-03-24 14:17:44 -0400

    Corrected SQL Schema

--- server/database/stock.sql
+++ server/database/stock.sql
@@ -6,13 +6,13 @@ DROP TABLE IF EXISTS `Stock_Update`;
 DROP TABLE IF EXISTS `User`;
 DROP TABLE IF EXISTS `Watchlist`;
 
-DROP TABLE IF EXISTS `Transaction`;
-DROP TABLE IF EXISTS `User_Transaction`;
-DROP TABLE IF EXISTS `Group_Transaction`;
-
 DROP TABLE IF EXISTS `Groups`;
 DROP TABLE IF EXISTS `Group_Users`;
 DROP TABLE IF EXISTS `Group_Stock`;
+
+DROP TABLE IF EXISTS `Transaction`;
+DROP TABLE IF EXISTS `User_Transaction`;
+DROP TABLE IF EXISTS `Group_Transaction`;
 
 -- --------------------------------------------------------
 
@@ -86,6 +86,56 @@ CREATE TABLE IF NOT EXISTS `Watchlist` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Groups`
+--
+
+CREATE TABLE IF NOT EXISTS `Groups` (
+  -- Attributes
+  `group_id` int(11) NOT NULL,
+  `balance` DECIMAL(12, 10) NOT NULL,
+
+  -- Keys
+  PRIMARY KEY (`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Group_Users`
+--
+
+CREATE TABLE IF NOT EXISTS `Group_Users` (
+  -- Attributes
+  `group_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+
+  -- Keys
+  PRIMARY KEY (`group_id`, `user_id`)
+  FOREIGN KEY (`user_id`) REFERENCES User(`user_id`),
+  FOREIGN KEY (`group_id`) REFERENCES Groups(`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Group_Stock`
+--
+
+CREATE TABLE IF NOT EXISTS `Group_Stock` (
+  -- Attributes
+  `group_id` int(11) NOT NULL,
+  `stock_id` int(11) NOT NULL,
+
+  -- Keys
+  PRIMARY KEY (`group_id`, `stock_id`)
+  FOREIGN KEY (`stock_id`) REFERENCES Stock(`stock_id`),
+  FOREIGN KEY (`group_id`) REFERENCES Groups(`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+
+--
 -- Table structure for table `Transaction`
 --
 
@@ -139,55 +189,6 @@ CREATE TABLE IF NOT EXISTS `Group_Transaction` (
   FOREIGN KEY (`group_id`) REFERENCES Group(`group_id`),
   FOREIGN KEY (`stock_id`) REFERENCES Stock(`stock_id`),
   FOREIGN KEY (`transaction_id`) REFERENCES Transaction(`transaction_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Groups`
---
-
-CREATE TABLE IF NOT EXISTS `Groups` (
-  -- Attributes
-  `group_id` int(11) NOT NULL,
-  `balance` DECIMAL(12, 10) NOT NULL,
-
-  -- Keys
-  PRIMARY KEY (`group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Group_Users`
---
-
-CREATE TABLE IF NOT EXISTS `Group_Users` (
-  -- Attributes
-  `group_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-
-  -- Keys
-  PRIMARY KEY (`group_id`, `user_id`)
-  FOREIGN KEY (`user_id`) REFERENCES User(`user_id`),
-  FOREIGN KEY (`group_id`) REFERENCES Groups(`group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Group_Stock`
---
-
-CREATE TABLE IF NOT EXISTS `Group_Stock` (
-  -- Attributes
-  `group_id` int(11) NOT NULL,
-  `stock_id` int(11) NOT NULL,
-
-  -- Keys
-  PRIMARY KEY (`group_id`, `stock_id`)
-  FOREIGN KEY (`stock_id`) REFERENCES Stock(`stock_id`),
-  FOREIGN KEY (`group_id`) REFERENCES Groups(`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
