7c6a89791ab7f571f27abad7cb6c3e6a49b65290
Weiyan  Hu <austin@Weiyans-MacBook-Air.local>
2021-03-25 09:57:46 -0400

    allows buy page to view all stocks

--- server/app.py
+++ server/app.py
@@ -1,18 +1,28 @@
 import yaml
 import mysql.connector
-
 from flask import Flask, request, redirect, render_template
+import os
 
 # End Imports--------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Load Database Configuration
-db = yaml.load(open(os.environ['DATABASE_CONFIG']), yaml.Loader)
+db = yaml.load(open("db.yaml"), yaml.Loader)
 
 # Establish Database Connection
+
 cnx = mysql.connector.connect(user = db['mysql_user'], password = db['mysql_password'],
                               host = db['mysql_host'], database = db['mysql_db'])
+cursor = cnx.cursor()
 
 # End Database Connection--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Queries
+
+#query for getting all current stock information
+get_stock = "SELECT stock_id,name,price,share FROM Stock"
+
+# End Queries --------------------------------------------------------------------------------------------------------------------------------------------
 
 # Create Flask Application
 app = Flask(__name__)
@@ -35,6 +45,43 @@ def home():
 # Page to display when user clicks buy stock
 @app.route('/buy')
 def buy():
+    #remove buy.html and create it. Make sure buy.html is an empty file
+    #every time before writing to it
+    if os.path.exists("templates/buy.html"):
+        os.remove("templates/buy.html")
+    buy_page = open("templates/buy.html", "x")
+
+    #buy_template.html contains code for asking user which stock_id
+    #they want to buy and how many they want to buy
+    buy_template_page = open("templates/buy_template.html", "r")
+
+    #loop through all lines of code into buy_template.html
+    for line in buy_template_page:
+        #write each row into buy.html
+        buy_page.write(line)
+
+    #execute the query for getting all stock information
+    cursor.execute(get_stock)
+    table = ""
+
+    #load all stock information into table variable
+    for stock_id,name,price,share in cursor:
+        table = table + "<tr>\n"
+        table = table + "<td>" + str(stock_id) + "</td>"
+        table = table + "<td>" + str(name) + "</td>"
+        table = table + "<td>" + str(price) + "</td>"
+        table = table + "<td>" + str(share) + "</td>"
+        table = table + "</tr>\n"
+
+    #load table value into buy_page
+    buy_page.write(table)
+
+
+    #end of html
+    buy_page.write("</table>")
+    buy_page.write("\n</html>")
+    buy_page.close()
+    #copy all of the code inside buy_template.html into buy.html
     return (render_template('buy.html'))
 
 # Page to display when user clicks sell stock
