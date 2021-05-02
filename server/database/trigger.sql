0eabc97c97d4adc150c30d2e1ad73ac877f1b506
schikyal <42979122+schikyal@users.noreply.github.com>
2021-05-02 18:15:49 -0400

    triggers load in to mysql now

--- server/database/trigger.sql
+++ server/database/trigger.sql
@@ -18,11 +18,11 @@ SET @amount = (SELECT amount
 IF @type = 1 THEN
     UPDATE User_Stock
 	SET shares = amount + @amount
-	WHERE stock_id = new.stock_id AND user_id = new.user_id
+	WHERE stock_id = new.stock_id AND user_id = new.user_id;
 ELSE
     UPDATE User_Stock
 	SET shares = amount - @amount
-	WHERE stock_id = new.stock_id AND user_id = new.user_id
+	WHERE stock_id = new.stock_id AND user_id = new.user_id;
 END IF;
 
 END //
@@ -38,22 +38,22 @@ FOR EACH ROW
 
 BEGIN
 
-SET @type = (SELECT type
-                FROM Group_Transaction
-                WHERE transaction_id = new.transaction_id);
-SET @amount = (SELECT amount
-                FROM Transaction
-                WHERE transaction_id = new.transaction_id);
+    SET @type = (SELECT type
+                    FROM Group_Transaction
+                    WHERE transaction_id = new.transaction_id);
+    SET @amount = (SELECT amount
+                    FROM Transaction
+                    WHERE transaction_id = new.transaction_id);
 
-IF @type = 1 THEN
-    UPDATE Group_Stock
-	SET shares = shares + @amount
-	WHERE stock_id = new.stock_id AND group_id = new.group_id
-ELSE
-    UPDATE Group_Stock
-	SET shares = shares - @amount
-	WHERE stock_id = new.stock_id AND group_id = new.group_id
-END IF;
+    IF @type = 1 THEN
+        UPDATE Group_Stock
+        SET shares = shares + @amount
+        WHERE stock_id = new.stock_id AND group_id = new.group_id;
+    ELSE
+        UPDATE Group_Stock
+        SET shares = shares - @amount
+        WHERE stock_id = new.stock_id AND group_id = new.group_id;
+    END IF;
 
 END //
 DELIMITER ;
